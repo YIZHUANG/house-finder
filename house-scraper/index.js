cc38376f6206f9e8bfc5ebad6d1cb8a897d39589
YIZHUANG <yi.zhuang1@outlook.com>
2019-07-18 02:39:13 +0300

    Added test emails

--- house-scraper/index.js
+++ house-scraper/index.js
@@ -1,13 +1,17 @@
 const puppeteer = require("puppeteer");
 const scrapWebsiteOne = require("./scrapWebsiteOne");
+const sendEmail = require("../utils/sendEmail");
 const { website_1_url } = process.env;
 
-module.exports = async (context) => {
+module.exports = async context => {
+  const { log } = context;
+  const timeStamp = new Date().toISOString();
+  log("Trigger time", timeStamp);
   const browser = await puppeteer.launch({
     headless: true,
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
-  const { log } = context;
+
   try {
     const websiteOneData = await scrapWebsiteOne(
       browser,
@@ -16,6 +20,7 @@ module.exports = async (context) => {
       log
     );
     log(`Got ${websiteOneData.length} of matches in total`);
+    await sendEmail(websiteOneData);
     context.res = {
       // status: 200, /* Defaults to 200 */
       body: websiteOneData,
--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,1 +1,42 @@
-// todos
+const nodemailer = require("nodemailer");
+
+const emailTemplate = houses => `
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+  <html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta name="viewport" content="width=device-width" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  </head>
+  <body itemscope itemtype="http://schema.org/EmailMessage">
+    ${houses.map(house => {
+      return `
+      <div class="card">
+        <div class="container">
+          <a href='${house.link}'><b>${house.address}</b></a> 
+          <p>${house.price}</p>
+          </div>
+      </div>
+      `;
+    })}
+  </body>
+</html>
+`;
+async function sendEmail(data) {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.emailUsername,
+      pass: process.env.emailPassword,
+    },
+  });
+  const timeStamp = new Date().toISOString();
+  const mailOptions = {
+    from: process.env.emailUsername,
+    to: process.env.emailReceiver,
+    subject: `${timeStamp} ${process.env.emailSubject}`,
+    html: emailTemplate(data),
+  };
+  await transporter.sendMail(mailOptions);
+}
+
+module.exports = sendEmail;
